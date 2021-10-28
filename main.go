92de0dedd9957afa95718d2cb77d975aa5c851d3
Lucasmercado101 <lucasmercado101@gmail.com>
2021-10-28 15:15:10 -0300

    added TODO, handled preflights

--- main.go
+++ main.go
@@ -54,11 +54,17 @@ func main() {
 	// enable CORS
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// TODO: learn how to handle all this better
 			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 			next.ServeHTTP(w, r)
+
 		})
 	})
 
