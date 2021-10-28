36270e30d486b3d9f6d3e00cf9ed5c210c0d1e6f
Lucasmercado101 <lucasmercado101@gmail.com>
2021-10-28 13:26:36 -0300

    on login set cookie and store session id

--- endpoints/auth.go
+++ endpoints/auth.go
@@ -3,12 +3,13 @@ package endpoints
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"quickstart/database"
 
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -36,8 +37,11 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	var hashedDBPass string
-	err = database.Db.QueryRow("SELECT password FROM users WHERE username = $1", creds.Username).Scan(&hashedDBPass)
+	var dbUser struct {
+		id       string
+		password string
+	}
+	err = database.Db.QueryRow("SELECT id, password FROM users WHERE username = $1", creds.Username).Scan(&dbUser.id, &dbUser.password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			w.WriteHeader(http.StatusNotFound)
@@ -46,9 +50,19 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	if checkPasswordHash(creds.Password, hashedDBPass) {
-		// TODO: cookie
-		fmt.Println("Password is correct")
+	if checkPasswordHash(creds.Password, dbUser.password) {
+		cookie := http.Cookie{
+			Name:  "sid",
+			Value: uuid.New().String(),
+			Path:  "/",
+		}
+		http.SetCookie(w, &cookie)
+		_, err := database.Db.Exec("INSERT INTO sessions (user_id, session_id, expiration) VALUES ($1, $2, $3)", dbUser.id, cookie.Value, time.Now().Add(time.Minute*2))
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 	}
@@ -79,5 +93,4 @@ func Register(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusForbidden) // https://stackoverflow.com/a/34458500
 		}
 	}
-
 }
