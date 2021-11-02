da185a6681e970045dd59a07d6456da71e7b2f5e
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-02 16:16:01 -0300

    WIP: created

---
+++ endpoints/itinerary.go
@@ -0,0 +1,49 @@
+package endpoints
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type itineraryInput struct {
+	CityId     *int     `json:"cityId"`
+	Title      string   `json:"title"`
+	Duration   *int     `json:"duration"`
+	Price      *int     `json:"price"`
+	Tags       []string `json:"tags"`
+	Activities []string `json:"activities"`
+}
+
+func Itinerary(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+	var input itineraryInput
+	json.NewDecoder(r.Body).Decode(&input)
+
+	// Validation
+	if input.CityId == nil {
+		http.Error(w, "Missing cityId", http.StatusBadRequest)
+		return
+	}
+	if input.Title == "" {
+		http.Error(w, "Missing title", http.StatusBadRequest)
+		return
+	}
+	if input.Duration == nil {
+		http.Error(w, "Missing duration", http.StatusBadRequest)
+		return
+	}
+	if input.Price == nil {
+		http.Error(w, "Missing price", http.StatusBadRequest)
+		return
+	}
+	if len(input.Tags) == 0 {
+		http.Error(w, "Missing tags", http.StatusBadRequest)
+		return
+	}
+	if len(input.Activities) == 0 {
+		http.Error(w, "Missing activities", http.StatusBadRequest)
+		return
+	}
+
+}
