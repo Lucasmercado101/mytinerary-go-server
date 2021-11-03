b65886a073a47ffb280aa6adc2ee65bd28b84c47
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-03 12:27:26 -0300

    added Itinerary struct type

--- database/database.go
+++ database/database.go
@@ -3,3 +3,14 @@ package database
 import "database/sql"
 
 var Db *sql.DB
+
+type Itinerary struct {
+	Id         int
+	Title      string
+	Creator    int
+	Time       string
+	Price      string
+	Activities []string
+	Hashtags   []string
+	CityId     int
+}
