14239cda3506836404bd068ea0abf83c1e55241e
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-03 13:27:55 -0300

    added CityItineraries endpoint

--- endpoints/city.go
+++ endpoints/city.go
@@ -8,12 +8,28 @@ import (
 	"quickstart/database"
 
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
 )
 
 type CityJSON struct {
 	Id      string `json:"id"`
 	Name    string `json:"name"`
 	Country string `json:"country"`
+}
+
+type itinerary struct {
+	Id         int              `json:"id"`
+	Title      string           `json:"title"`
+	Time       int              `json:"time"`
+	Price      int              `json:"price"`
+	Activities pq.StringArray   `json:"activities"`
+	Hashtags   pq.StringArray   `json:"hashtags"`
+	Creator    itineraryCreator `json:"creator"`
+}
+
+type itineraryCreator struct {
+	User_id     int    `json:"userId"`
+	Profile_pic string `json:"profilePic"`
 }
 
 func City(w http.ResponseWriter, r *http.Request) {
@@ -71,4 +87,55 @@ func City(w http.ResponseWriter, r *http.Request) {
 
 		w.WriteHeader(http.StatusNoContent)
 	}
+}
+
+func CityItineraries(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+	id := mux.Vars(r)["cityId"]
+
+	// TODO: validation
+
+	rows, err := database.Db.Query(`
+	SELECT id,
+		title,
+		time,
+		price,
+		activities,
+		hashtags,
+		user_id,
+    	profile_pic
+	FROM itinerary
+		INNER JOIN (
+			SELECT id as user_id,
+				profile_pic
+			FROM users
+		) AS users ON itinerary.creator = users.user_id
+	WHERE itinerary.id = $1`, id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		rows.Close()
+		return
+	}
+
+	defer rows.Close()
+
+	var itineraries []itinerary
+
+	for rows.Next() {
+
+		var itinerary itinerary
+
+		err := rows.Scan(&itinerary.Id, &itinerary.Title,
+			&itinerary.Time, &itinerary.Price,
+			&itinerary.Activities, &itinerary.Hashtags,
+			&itinerary.Creator.User_id, &itinerary.Creator.Profile_pic)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		itineraries = append(itineraries, itinerary)
+	}
+
+	json.NewEncoder(w).Encode(itineraries)
 }
