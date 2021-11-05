16f9301cad6325733e8ceed60ba55a6502178c9f
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-05 13:51:13 -0300

    WIP: created ItineraryComment endpoint

--- endpoints/itinerary.go
+++ endpoints/itinerary.go
@@ -31,3 +31,74 @@ func Itinerary(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(itinerary)
 }
+
+type itineraryCommentInput struct {
+	Content  string `json:"content"`
+	AuthorId int    `json:"authorId"`
+}
+
+func ItineraryComment(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+
+	itineraryId := mux.Vars(r)["itineraryId"]
+
+	var input itineraryCommentInput
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Validation
+	if input.Content == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// TODO: more validation
+
+	tx, err := database.Db.Begin()
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var newCommentId int
+
+	err = tx.QueryRow(`
+	INSERT INTO itinerary_comment (author_id, comment)
+	VALUES ($1, $2)
+	RETURNING id
+	`, input.AuthorId, input.Content).Scan(&newCommentId)
+
+	if err != nil {
+		tx.Rollback()
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err != nil {
+		tx.Rollback()
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = tx.Exec(`
+	INSERT INTO itinerary_comments (itinerary_id, comment_id)
+	VALUES ($1, $2)
+	`, itineraryId, newCommentId)
+
+	if err != nil {
+		tx.Rollback()
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	tx.Commit()
+
+}
