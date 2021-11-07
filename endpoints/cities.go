33a5393abaa7d9b79945fc8a8084b5cbba568de3
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-07 11:35:25 -0300

    post can take other content types

--- endpoints/cities.go
+++ endpoints/cities.go
@@ -3,6 +3,7 @@ package endpoints
 import (
 	"encoding/json"
 	"log"
+	"mime"
 	"net/http"
 	"quickstart/database"
 )
@@ -41,21 +42,45 @@ func Cities(w http.ResponseWriter, r *http.Request) {
 		}
 
 	case "POST":
-		if r.Header.Get("Content-Type") != "application/json" {
-			http.Error(w, "Content-Type must be 'application/json'", http.StatusUnsupportedMediaType)
+		mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
 		var city CityJSON
-		if err := json.NewDecoder(r.Body).Decode(&city); err != nil {
-			http.Error(w, "Invalid JSON", http.StatusBadRequest)
-			return
-		}
-		log.Printf("City: %+v", city)
 
-		err := database.Db.QueryRow("INSERT INTO city (name, country) VALUES ($1, $2) RETURNING *", city.Name, city.Country).Scan(&city.Id, &city.Name, &city.Country)
+		switch mediaType {
+		case "application/json":
+			if err := json.NewDecoder(r.Body).Decode(&city); err != nil {
+				http.Error(w, "Invalid JSON", http.StatusBadRequest)
+				return
+			}
+
+		case "application/x-www-form-urlencoded":
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, "Invalid form", http.StatusBadRequest)
+				return
+			}
+			city.Name = r.Form.Get("name")
+			city.Country = r.Form.Get("country")
+
+		case "multipart/form-data":
+			if err := r.ParseMultipartForm(32 << 20); err != nil {
+				http.Error(w, "Invalid form", http.StatusBadRequest)
+				return
+			}
+			city.Name = r.Form.Get("name")
+			city.Country = r.Form.Get("country")
+		}
+
+		log.Printf("%+v\n", city)
+
+		err = database.Db.QueryRow("INSERT INTO city (name, country) VALUES ($1, $2) RETURNING *", city.Name, city.Country).Scan(&city.Id, &city.Name, &city.Country)
 		if err != nil {
-			panic(err)
+			log.Print(err)
+			w.WriteHeader(http.StatusInternalServerError)
 		}
 
 		json.NewEncoder(w).Encode(city)
